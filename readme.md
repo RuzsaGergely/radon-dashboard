ace1c6de3883392934d012242da02552505c182b
Ruzsa Gergely <ruzsa.gergely@gmail.com>
2020-04-12 19:09:13 +0200

    A lot of modules ready to go

--- readme.md
+++ readme.md
@@ -1,1 +1,130 @@
-## Radon dashboard
+# Radon dashboard
+
+## Install
+
+## Requests
+
+### changeMap
+
+POST */api/changeMap/[server_id]*
+
+Request body:
+
+```json
+{
+	"map": "de_dust2"
+}
+```
+
+Available maps:
+
+- de_dust2
+- de_inferno
+- de_train
+- de_mirage
+- de_nuke
+- de_overpass
+- de_verigo
+
+
+
+### changeFlag
+
+POST */api/changeMap/[server_id]*
+
+Request body:
+
+```json
+{
+	"team": 1,
+	"country": "hu"
+}
+```
+
+Team 1 is Counter-terrorists and team 2 is Terrorists.
+
+For the "country" parameter, you must supply an 2 character long country ISO code. For all ISO codes, there are a [website](https://www.iban.com/country-codes). Don't forget, you need 2 character long ISO codes!
+
+
+
+### changeTeamname
+
+POST */api/changeTeamname/[server_id]*
+
+Request body:
+
+```json
+{
+	"team": 1,
+	"name": "ITdept"
+}
+```
+
+Team 1 is Counter-terrorists and team 2 is Terrorists.
+
+
+
+### getTasks
+
+GET */api/getTasks/[server_id]*
+
+This will return the jobs on the list, for the specified server.
+
+
+
+### doneTask
+
+POST */api/doneTask/[server_id]*
+
+Request body:
+
+```json
+{
+	"tasknum": 1
+}
+```
+
+This will remove the task from the active jobs ('jobs' table) and move to the 'old_jobs' table in case, when supervision needed.
+
+
+
+### setPoints
+
+POST */api/setPoints/[server_id]*
+
+Request body:
+
+```json
+{
+	"ctpoint": 1,
+	"tpoint": 2
+}
+```
+
+Both argument should be provided.
+
+
+
+### getPoints
+
+GET */api/getPoints/*
+
+No request body.
+
+
+
+### changeStats
+
+POST */api/changeStats/[server_id]*
+
+Request body:
+
+```json
+{
+	"txt": "\"Hello\"",
+	"team1": "\"Szia\"",
+    "team2": "\"Ciao\""
+}
+```
+
+Team 1 is Counter-terrorists and team 2 is Terrorists. All argument should be provided, else it will be rejected.
--- site/application/controllers/Api.php
+++ site/application/controllers/Api.php
@@ -9,12 +9,83 @@ class Api extends CI_Controller
 		$this->respError(403, "Forbidden", "Nothing here");
 	}
 
-	public function demo($server=null){
+    public function changeTeamname($server=null){
+        if($server!=null && is_numeric($server)){
 
-	    if($server!=null && is_numeric($server)){
             if($_SERVER['REQUEST_METHOD'] == "POST") {
+
                 $rawdata = file_get_contents("php://input");
                 $decoded = json_decode($rawdata, true);
+
+                if (isset($decoded["name"], $decoded["team"]) && !empty($decoded["team"] . $decoded["name"])){
+                    if($decoded["team"] == 1){
+                        $sql = "INSERT INTO `jobs`(`servernum`, `task`) VALUES (?,?)";
+                        $this->db->query($sql, array($server, "mp_teamname_1 " . $decoded["name"]));
+
+                        if($this->db->affected_rows() > 0){
+                            $this->respError(200, "OK", "Task sent");
+                        } else {
+                            $this->respError(500, "Internal error", "Task not sent");
+                        }
+                    } else if($decoded["team"] == 2){
+                        $sql = "INSERT INTO `jobs`(`servernum`, `task`) VALUES (?,?)";
+                        $this->db->query($sql, array($server, "mp_teamname_2 " . $decoded["name"]));
+
+                        if($this->db->affected_rows() > 0){
+                            $this->respError(200, "OK", "Task sent");
+                        } else {
+                            $this->respError(500, "Internal error", "Task not sent");
+                        }
+                    } else{
+                        $this->respError(400, "Bad request", "No or invalid team");
+                    }
+                } else {
+                    $this->respError(400, "Bad request", "No or invalid name or team");
+                }
+            } else {
+                $this->respError(400, "Bad request", "Invalid request type");
+            }
+        } else {
+            $this->respError(400, "Bad request", "Invalid or no argument");
+        }
+    }
+
+    public function changeFlag($server=null){
+
+        if($server!=null && is_numeric($server)){
+
+            if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+                $rawdata = file_get_contents("php://input");
+                $decoded = json_decode($rawdata, true);
+
+                if (isset($decoded["country"], $decoded["team"]) && strlen($decoded["country"]) == 2 && !empty($decoded["team"]) && !is_numeric($decoded["country"])){
+
+                    if($decoded["team"] == 1){
+                        $sql = "INSERT INTO `jobs`(`servernum`, `task`) VALUES (?,?)";
+                        $this->db->query($sql, array($server, "mp_teamflag_1 " . $decoded["country"]));
+
+                        if($this->db->affected_rows() > 0){
+                            $this->respError(200, "OK", "Task sent");
+                        } else {
+                            $this->respError(500, "Internal error", "Task not sent");
+                        }
+                    } else if($decoded["team"] == 2){
+                        $sql = "INSERT INTO `jobs`(`servernum`, `task`) VALUES (?,?)";
+                        $this->db->query($sql, array($server, "mp_teamflag_2 " . $decoded["country"]));
+
+                        if($this->db->affected_rows() > 0){
+                            $this->respError(200, "OK", "Task sent");
+                        } else {
+                            $this->respError(500, "Internal error", "Task not sent");
+                        }
+                    } else{
+                        $this->respError(400, "Bad request", "No or invalid team");
+                    }
+
+                } else {
+                    $this->respError(400, "Bad request", "No or invalid country");
+                }
             } else {
                 $this->respError(400, "Bad request", "Invalid request type");
             }
@@ -24,18 +95,205 @@ class Api extends CI_Controller
 
     }
 
-    public function changeTeamname($server=null){
-
-    }
-
-    public function changeFlag($server=null){
-
-    }
-
     public function changeMap($server=null){
 
+	    $maps = array(
+	        "de_dust2",
+            "de_inferno",
+            "de_train",
+            "de_mirage",
+            "de_nuke",
+            "de_overpass",
+            "de_vertigo"
+        );
+
+        if($server!=null && is_numeric($server)){
+
+            if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+                $rawdata = file_get_contents("php://input");
+                $decoded = json_decode($rawdata, true);
+
+                if (isset($decoded["map"]) && in_array($decoded["map"], $maps)){
+
+                    $sql = "INSERT INTO `jobs`(`servernum`, `task`) VALUES (?,?)";
+                    $this->db->query($sql, array($server, "changelevel " . $decoded["map"]));
+
+                    if($this->db->affected_rows() > 0){
+                        $this->respError(200, "OK", "Task sent");
+                    } else {
+                        $this->respError(500, "Internal error", "Task not sent");
+                    }
+
+                } else {
+                    $this->respError(400, "Bad request", "No or invalid map");
+                }
+            } else {
+                $this->respError(400, "Bad request", "Invalid request type");
+            }
+        } else {
+            $this->respError(400, "Bad request", "Invalid or no argument");
+        }
+
     }
-	
+
+    public function doneTask($server=null){
+
+        if($server!=null && is_numeric($server)){
+            if($_SERVER['REQUEST_METHOD'] == "POST") {
+                $rawdata = file_get_contents("php://input");
+                $decoded = json_decode($rawdata, true);
+
+                if(isset($decoded["tasknum"]) && !empty($decoded["tasknum"]) && is_numeric($decoded["tasknum"])){
+                    $sql = "INSERT INTO `old_jobs` SELECT * FROM `jobs` WHERE `tasknum` = ?";
+                    $this->db->query($sql, array($decoded["tasknum"]));
+                    $sql = "DELETE FROM `jobs` WHERE `tasknum` = ?";
+                    $this->db->query($sql, array($decoded["tasknum"]));
+                    $this->respError(200, "OK", "Task marked as done");
+                } else {
+                    $this->respError(400, "Bad request", "Invalid or no tasknum");
+                }
+            } else {
+                $this->respError(400, "Bad request", "Invalid request type");
+            }
+        } else {
+            $this->respError(400, "Bad request", "Invalid or no argument");
+        }
+
+    }
+
+    public function getTasks($server=null){
+
+        if($server!=null && is_numeric($server)){
+            if($_SERVER['REQUEST_METHOD'] == "GET") {
+                $sql = "SELECT * FROM `jobs` WHERE `servernum`= ?";
+                $query = $this->db->query($sql, array($server));
+
+                $response = array();
+                foreach ($query->result() as $row)
+                {
+                    array_push($response, array(
+                        "tasknum" =>$row->tasknum,
+                        "task" => $row->task
+                    ));
+                }
+                header('Content-Type: application/json');
+                $json_response = json_encode($response);
+                echo $json_response;
+            } else {
+                $this->respError(400, "Bad request", "Invalid request type");
+            }
+        } else {
+            $this->respError(400, "Bad request", "Invalid or no argument");
+        }
+
+    }
+
+    public function setPoints($server=null){
+
+        if($server!=null && is_numeric($server)){
+            if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+                $rawdata = file_get_contents("php://input");
+                $decoded = json_decode($rawdata, true);
+
+                if(isset($decoded["ctpoint"], $decoded["tpoint"]) && !empty($decoded["ctpoint"] . $decoded["tpoint"]) && is_numeric($decoded["ctpoint"]) && is_numeric($decoded["tpoint"])){
+                    $sql = "SELECT * FROM `server_points` WHERE `servernum`= ?";
+                    $this->db->query($sql, array($server));
+
+                    if ($this->db->affected_rows() > 0){
+                        $sql = "UPDATE `server_points` SET `t_point`=?,`ct_point`=? WHERE `servernum`=?";
+                        $this->db->query($sql, array($decoded["tpoint"], $decoded["ctpoint"], $server));
+                        if ($this->db->affected_rows() > 0){
+                            $this->respError(200, "OK", "Points updated");
+                        } else {
+                            $this->respError(500, "Internal error", "Points not updated");
+                        }
+                    } else {
+                        $sql = "INSERT INTO `server_points`(`servernum`, `t_point`, `ct_point`) VALUES (?,?,?)";
+                        $this->db->query($sql, array($server, $decoded["tpoint"], $decoded["ctpoint"]));
+                        if ($this->db->affected_rows() > 0){
+                            $this->respError(200, "OK", "Points updated and server registered");
+                        } else {
+                            $this->respError(500, "Internal error", "Points not updated and/or not registered");
+                        }
+                    }
+
+                } else {
+                    $this->respError(400, "Bad request", "Missing or invalid point");
+                }
+
+
+            } else {
+                $this->respError(400, "Bad request", "Invalid request type");
+            }
+        } else {
+            $this->respError(400, "Bad request", "Invalid or no argument");
+        }
+
+    }
+
+    public function getPoints(){
+        if($_SERVER['REQUEST_METHOD'] == "GET"){
+
+            $query = $this->db->query("SELECT * FROM `server_points`");
+
+            $response = array();
+
+            foreach ($query->result() as $row)
+            {
+                array_push($response, array(
+                    "server" => $row->servernum,
+                    "tpoint" =>$row->t_point,
+                    "ctpoint" => $row->ct_point
+                ));
+            }
+
+            header('Content-Type: application/json');
+            $json_response = json_encode($response);
+            echo $json_response;
+        }
+    }
+
+    public function changeStats($server=null){
+        if($server!=null && is_numeric($server)) {
+            if ($_SERVER['REQUEST_METHOD'] == "POST") {
+                $rawdata = file_get_contents("php://input");
+                $decoded = json_decode($rawdata, true);
+
+                if(isset($decoded["txt"], $decoded["team1"], $decoded["team2"]) && !empty($decoded["txt"] . $decoded["team1"] . $decoded["team2"])){
+                    $count = 0;
+                    $sql = "INSERT INTO `jobs`(`servernum`, `task`) VALUES (?,?)";
+                    $this->db->query($sql, array($server, "mp_teammatchstat_txt " . $decoded["txt"]));
+                    if($this->db->affected_rows() > 0){
+                        $count+=1;
+                    }
+                    $sql = "INSERT INTO `jobs`(`servernum`, `task`) VALUES (?,?)";
+                    $this->db->query($sql, array($server, "mp_teammatchstat_1 " . $decoded["team1"]));
+                    if($this->db->affected_rows() > 0){
+                        $count+=1;
+                    }
+                    $sql = "INSERT INTO `jobs`(`servernum`, `task`) VALUES (?,?)";
+                    $this->db->query($sql, array($server, "mp_teammatchstat_2 " . $decoded["team2"]));
+                    if($this->db->affected_rows() > 0){
+                        $count+=1;
+                    }
+                    if($count == 3){
+                        $this->respError(200, "OK", "3/3 task sent");
+                    } else {
+                        $this->respError(500, "Internal error", $count . "/3 task sent");
+                    }
+                } else {
+                    $this->respError(400, "Bad request", "Invalid or empty input");
+                }
+            }else {
+                $this->respError(400, "Bad request", "Invalid request type");
+            }
+        } else {
+            $this->respError(400, "Bad request", "Invalid or no argument");
+        }
+    }
+
 	// Not reachable from browser or through API request
 	private function respError($httpcode, $httpstatus, $errormessage)
 	{
